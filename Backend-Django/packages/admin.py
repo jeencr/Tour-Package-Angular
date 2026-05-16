1a4a969dac6956ecbb877d96486edda9404aef3a
Jeevan C R <145118797+jeencr@users.noreply.github.com>
2026-05-16 15:12:24 +0530

    added package created the tables architecture set

--- Backend-Django/packages/admin.py
+++ Backend-Django/packages/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Category)
+admin.site.register(Destination)
+admin.site.register(DestinationImage)
+admin.site.register(Package)
+admin.site.register(PackageImage)
--- Backend-Django/packages/models.py
+++ Backend-Django/packages/models.py
@@ -1,7 +1,53 @@
 from django.db import models
+from users.models import Providers
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
     def __str__(self):
         return self.name
+    
+class Destination(models.Model):
+
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    latitude = models.CharField(max_length=100)
+    longitude = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class DestinationImage(models.Model):
+
+    destination = models.ForeignKey(Destination,on_delete=models.CASCADE,related_name='images')
+    image = models.ImageField(upload_to='destinations/')
+
+    def __str__(self):
+        return self.destination.name
+    
+
+class Package(models.Model):
+    destination = models.ForeignKey(Destination,on_delete=models.CASCADE)
+    provider = models.ForeignKey(Providers,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    package_name = models.CharField(max_length=100)
+    description = models.TextField()
+    amount = models.DecimalField(max_digits=10,decimal_places=2)
+    no_of_days = models.IntegerField()
+    no_of_nights = models.IntegerField()
+    no_of_adults = models.IntegerField()
+    no_of_children = models.IntegerField()
+    is_public = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.package_name
+    
+
+class PackageImage(models.Model):
+    package = models.ForeignKey(Package,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='package_image/')
+
+    def __str__(self):
+        return self.package.package_name
